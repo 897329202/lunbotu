4ceb680c7c55df67d327e77635a770f43efd3a7a
897329202 <897329202@qq.com>
2016-10-16 12:21:15 +0800

    第二

---
+++ yunxiaoer123/src/main/java/com/baidu/yunxiaoer/fragment/MyFragment.java
@@ -0,0 +1,10 @@
+package com.baidu.yunxiaoer.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2016-10-16.
+ */
+
+public class MyFragment extends Fragment {
+}
